f8c78c22bc894fad097a7e7fb6c767e454d0b00a
SaurabhSahu01 <saurabhsahu794@gmail.com>
2021-12-19 22:33:19 +0530

    JNU_SoE website for resources

---
+++ index.js
@@ -0,0 +1,33 @@
+slideshow();
+const themeBtn = document.getElementById("themeBtn");
+themeBtn.addEventListener('click',themeChange);
+
+const jnuLogo = document.getElementById("logo");
+
+function themeChange(){
+    let theme = document.querySelector('nav');
+
+    if(theme.classList.contains('bg-dark')){
+        theme.classList.remove('bg-dark','navbar-dark');
+        theme.classList.add('light','navbar-light');
+        themeBtn.src = 'moon-fill.svg';
+        jnuLogo.src = "jnu.png";
+    }
+    else{
+        theme.classList.remove('light','navbar-light');
+        theme.classList.add('bg-dark','navbar-dark');
+        themeBtn.src = 'icons8-sun.svg';
+        jnuLogo.src = 'jnu2.gif';
+    }
+}
+
+let image = document.getElementById('image');
+let count = 0; 
+function slideshow(){
+    let array = ['pic1.jpeg', 'pic2.jpeg', 'pic3.jpeg', 'pic4.jpeg', 'pic5.jpeg'];
+    setInterval(function () {
+        let i = count % 3;
+        image.src = array[i];
+        count++;
+    }, 1500);
+}
