ec63a52a7a7585f50a1f54698e4883f489581aa4
SaurabhSahu01 <saurabhsahu794@gmail.com>
2021-12-22 21:46:57 +0530

    some changes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const themeBtn = document.getElementById("themeBtn");
 themeBtn.addEventListener('click',themeChange);
 
 const jnuLogo = document.getElementById("logo");
+const soeText = document.getElementById("schoolOfEngineering");
 
 function themeChange(){
     let theme = document.querySelector('nav');
@@ -10,14 +11,16 @@ function themeChange(){
     if(theme.classList.contains('bg-dark')){
         theme.classList.remove('bg-dark','navbar-dark');
         theme.classList.add('light','navbar-light');
-        themeBtn.innerHTML = `<button type="button" class="btn btn-light" style="background-color: rgb(255,255,255);">Dark Mode<img src="moon-fill.svg" style="margin-left: 20px;"></button>`;
+        themeBtn.innerHTML = `<button type="button" class="btn btn-light" style="background-color: rgb(255,255,255);">Dark Mode<img src="moon-fill.svg" style="margin-left: 8px;"></button>`;
         jnuLogo.src = "jnu.png";
+        soeText.style.color = "black";
     }
     else{
         theme.classList.remove('light','navbar-light');
         theme.classList.add('bg-dark','navbar-dark');
-        themeBtn.innerHTML = `<button type="button" class="btn btn-dark">Light Mode<img src="icons8-sun.svg" style="margin-left: 20px;"></button>`;
+        themeBtn.innerHTML = `<button type="button" class="btn btn-dark">Light Mode<img src="icons8-sun.svg" style="margin-left: 8px;"></button>`;
         jnuLogo.src = 'jnu2.gif';
+        soeText.style.color = "white";
     }
 }
 
